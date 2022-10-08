bb4725a4e8e21cc81c84729d1939ea9f81d58216
MD INZAMAM UL HOQ <82029738+Inzamam-Machine00Learning@users.noreply.github.com>
2022-10-08 15:54:13 +0100

    Create code.rb

---
+++ code.rb
@@ -0,0 +1,12 @@
+puts "Hello World"
+class HelloWorld
+   def initialize(name)
+      @name = name.capitalize
+   end
+   def sayHi
+      puts "Hello #{@name}!"
+   end
+end
+
+hello = HelloWorld.new("World")
+hello.sayHi
